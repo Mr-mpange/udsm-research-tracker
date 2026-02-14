e18a1341047e968d2660a25d169353503c29e5d8
Mr-mpange <kilindosaid771@gmail.com>
2026-02-14 11:32:40 +0300

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/udsm-research-tracker">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/dashboard" element={<Dashboard />} />
